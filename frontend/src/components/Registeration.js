3b3b4a74b99ee19abcd7ec8f35624de42563c244
Eetu Häkkinen <eetu.hakkinen@netti.fi>
2019-05-06 13:59:25 +0300

    1h user registeration form

---
+++ frontend/src/components/Registeration.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { registerUser } from '../services/userService';
+import { withRouter } from 'react-router-dom';
+
+//**Page which handles user registeration */
+const Registeration = (props) => {
+    // when register-button was clicked
+    onSubmit = async (e) => {
+        e.preventDefault();
+        const user = {
+            username: e.target.username.value,
+            name: e.target.name.value,
+            password: e.target.password.value
+        }
+        // send user registeration for backend
+        var regUser = await registerUser(user);
+        if (regUser.status === 200) {
+            // if registeration was OK, redirect to login form
+            props.history.push('/login');
+        }
+    }
+
+    return (
+        <div>
+            <h1>Registeration form</h1>
+            <form onSubmit={onSubmit}>
+                <p>Username</p>
+                <input type="text" name="username" />
+                <p>Name</p>
+                <input type="text" name="name" />
+                <p>Password</p>
+                <input type="password" name="password" />
+                <button type="submit">Register</button>
+            </form>
+        </div>
+    );
+}
+
+export default withRouter(Registeration);
---
+++ frontend/src/services/userService.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const baseUrl = 'http://localhost:3001/api/'
+
+//** Send user registeration post request to backend */
+export function registerUser(user) {
+    var res = await axios.post(baseUrl + 'user', user);
+    return res;
+}
